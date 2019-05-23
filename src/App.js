8fb2f5db8b7123ef92d366f525f74e720f9d15a9
AndriiTokar <tokar.andrii@gmail.com>
2019-05-23 11:00:30 +0300

    add theming switching

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Button from './Button';
 import Header from './Header';
@@ -6,11 +6,18 @@ import Footer from './Footer'
 import styled from 'styled-components';
 import { ThemeProvider } from 'styled-components';
 import backgroundImage from './balloons-flight.jpg';
+import backgroundImage2 from './architecture-buildings-business.jpg';
+import backgroundImage3 from './architectural-design.jpg';
+
+const THEME_DEFAULT = 'themeDefault';
+const THEME_LIGHT = 'themeLight';
+const THEME_SOLARIZED = 'themeSolarized';
 
 
-const theme = {
+
+const themeDefault = {
   COLOR: "white",
-  BACKGROUND: "#3232ff",
+  BACKGROUND: "black",
   FONT_SIZE: '32',
   HOVER_BTN: 'olive',
   HEADER_BG: '#282c34',
@@ -21,6 +28,37 @@ const theme = {
   FONT: 'Lobster',
   FONT_FAMILY: 'cursive',
   FOOTER_BG_COLOR: 'grey'
+};
+
+
+const themeLight = {
+  COLOR: "black",
+  BACKGROUND: "white",
+  FONT_SIZE: '32',
+  HOVER_BTN: 'olive',
+  HEADER_BG: 'white',
+  HEADER_COL: "black",
+  HEADER_APP_LINK: 'blue',
+  APP_DG_IMAGE: backgroundImage2,
+  MAIN_HEIGHT: '2300',
+  FONT: 'Lobster',
+  FONT_FAMILY: 'cursive',
+  FOOTER_BG_COLOR: 'white'
+};
+
+const themeSolarized = {
+  COLOR: "#d33682",
+  BACKGROUND: "#008f68",
+  FONT_SIZE: '32',
+  HOVER_BTN: 'olive',
+  HEADER_BG: '#008f68',
+  HEADER_COL: "white",
+  HEADER_APP_LINK: 'blue',
+  APP_DG_IMAGE: backgroundImage3,
+  MAIN_HEIGHT: '2560',
+  FONT: 'Lobster',
+  FONT_FAMILY: 'cursive',
+  FOOTER_BG_COLOR: '#fdf6e3'
 };
 
 const Main = styled.main`
@@ -42,10 +80,33 @@ const Main = styled.main`
 `;
 
 function App() {
+  const [theme, setTheme] = useState(themeDefault);
+  useEffect(() => {
+    setTheme(theme);
+  }, [theme]);
+
+  const handleChangeTheme = ({ target }) => {
+    const { value } = target;
+    switch (value) {
+      case THEME_DEFAULT:
+      default:
+        return setTheme(themeDefault);
+
+      case THEME_LIGHT: return setTheme(themeLight);
+      case THEME_SOLARIZED: return setTheme(themeSolarized);
+    }
+  }
+
   return (
     <ThemeProvider theme={theme}>
       <div className="App">
-        <Header></Header>
+        <Header theme={theme}>
+          <select onChange={handleChangeTheme}>
+            <option value={THEME_DEFAULT}>dark</option>
+            <option value={THEME_LIGHT}>light</option>
+            <option value={THEME_SOLARIZED}>solarized</option>
+          </select>
+        </Header>
         <Button text="First button with styled component"></Button>
         <Main>
           this is main
--- src/Header.js
+++ src/Header.js
@@ -2,21 +2,23 @@ import React from 'react';
 import styled from 'styled-components';
 import logo from './logo.svg';
 
-const Header = ({ className }) => (
-    
+const Header = ({ className, children }) => (
+
     <header className={className}>
         <a
-    
+
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
         >
-            Learn React
+            Switch themes
         </a>
         <p>
             Edit <code>src/App.js</code> and save to reload.
         </p>
         <img src={logo} className="App-logo" alt="logo" />
+
+        {children}
     </header>
 );
 
@@ -24,16 +26,22 @@ const StyledHeader = styled(Header)`
     background-color: ${props => props.theme.HEADER_BG};
     font-size: ${props => props.theme.FONT_SIZE}px;
     color: ${props => props.theme.COLOR};
-    background-color: #282c34;
     min-height: 10vh;
     display: flex;
     align-items: center;
     justify-content: space-between;
-    color: white;
     padding: 0 64px;
     font-family: ${props => props.theme.FONT}, ${props => props.theme.FONT_FAMILY};
     a {
-        color: ${props => props.theme.HEADER_APP_LINK}; 
+        color: ${props => props.theme.HEADER_APP_LINK};
+        text-decoration: none;
+    }
+
+    select{
+        padding: 2px;
+        font-size: ${props => props.theme.FONT_SIZE}px;
+        background-color:  ${props => props.theme.HEADER_BG};
+        color: ${props => props.theme.COLOR};
     }
 `;
 
